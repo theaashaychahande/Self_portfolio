3bed57171d17ee1b5690c925b156365042e9e0e0
Aashay Chahande <aashaychahande1717@gmail.com>
2025-07-18 21:53:34 +0530

    updated index file

---
+++ src/pages/Index.tsx
@@ -0,0 +1,19 @@
+import { Navigation } from '@/components/Navigation';
+import { HeroSection } from '@/components/HeroSection';
+import { AboutSection } from '@/components/AboutSection';
+import { ExperienceSection } from '@/components/ExperienceSection';
+import { SkillsSection } from '@/components/SkillsSection';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navigation />
+      <HeroSection />
+      <AboutSection />
+      <ExperienceSection />
+      <SkillsSection />
+    </div>
+  );
+};
+
+export default Index;
